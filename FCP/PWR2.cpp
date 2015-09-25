c4193200a051f164c6da22020e7c45030aa91e77
nikhilhassija <nikhil.hassija@gmail.com>
2015-09-25 09:01:07 +0530

    Added PWR2.cpp

---
+++ FCP/PWR2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	int t,n;
+	cin>>t;
+	while(t--)
+	{
+		cin>>n;
+		if(n)
+			cout<<"YES"<<endl;
+		else
+			cout<<"NO"<<endl;
+	}
+	return 0;
+}
