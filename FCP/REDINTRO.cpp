a403f583ee61f338a298a8eec1a7e23a87ce89a1
nikhilhassija <nikhil.hassija@gmail.com>
2015-09-27 22:33:48 +0530

    Added REDINTRO.cpp

---
+++ FCP/REDINTRO.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	int x,a;
+	cin>>x>>a;
+	cin>>a;
+	if(x<a)
+	{
+		cout<<"YES";
+		return 0;
+	}
+	cout<<"NO";
+	return 0;
+}
